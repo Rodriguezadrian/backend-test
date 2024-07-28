5ad251957026751ec04401f7ff3c031b4ae64867
Adrian Rodriguez <rgadrian.93@gmail.com>
2024-07-28 17:26:51 -0300

    update swagger error

--- server.js
+++ server.js
@@ -12,11 +12,25 @@ const swaggerDocument = YAML.load(
   path.join(__dirname, "docs", "documentation.yaml")
 );
 
+const allowedOrigins = ["https://frontend-test-delta-sooty.vercel.app"];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true,
+  })
+);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
 app.use(routes);
 
 app.get("/", (req, res) => res.json({ msg: "Welcome to Cellphone API!" }));
