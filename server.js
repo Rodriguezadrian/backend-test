b1b6e602233e8bdeaa3187167a7f38e05ce632d6
Adrian Rodriguez <rgadrian.93@gmail.com>
2024-07-28 15:17:23 -0300

    updated route for docs

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const swaggerUi = require("swagger-ui-express");
 const swaggerSpec = require("./swaggerOptions");
 const routes = require("./routes");
 const YAML = require("yamljs");
+const path = require('path');
 
 const app = express();
 
-const swaggerDocument = YAML.load("./documentation.yaml");
+const swaggerDocument = YAML.load(path.join(__dirname, "docs", "documentation.yaml"));
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(express.json());
