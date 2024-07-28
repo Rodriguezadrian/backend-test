52b0950eb3600140d9b55af874f83e90d14f0a88
Adrian Rodriguez <rgadrian.93@gmail.com>
2024-07-28 12:45:06 -0300

    add yaml doc

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,6 +3,32 @@ const productController = require("../controllers/productController");
 const router = express.Router();
 
 router.get("/", productController.index);
+/**
+ * @swagger
+ * /cellphones:
+ *   get:
+ *     summary: Obtener lista de teléfonos celulares
+ *     responses:
+ *       200:
+ *         description: Lista de teléfonos celulares
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     example: 1
+ *                   name:
+ *                     type: string
+ *                     example: "iPhone 12"
+ *                   price:
+ *                     type: number
+ *                     format: float
+ *                     example: 999.99
+ */
 router.get("/:id", productController.show);
 // router.post("/add", productController.store);
 // router.patch("/:id", productController.update);
--- server.js
+++ server.js
@@ -4,14 +4,18 @@ const cors = require("cors");
 const swaggerUi = require("swagger-ui-express");
 const swaggerSpec = require("./swaggerOptions");
 const routes = require("./routes");
+const YAML = require("yamljs");
 
 const app = express();
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+const swaggerDocument = YAML.load("./documentation.yaml");
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(routes);
+
 app.get("/", (req, res) => res.json({ msg: "Welcome to Cellphone API!" }));
 
 app.listen(process.env.APP_PORT, () => {
