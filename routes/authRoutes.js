8782cf583bdf47fc35e12d243f79ec66ed24b5f8
Adrian Rodriguez <rgadrian.93@gmail.com>
2024-07-28 11:25:05 -0300

    update routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,6 +2,36 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
 
+/**
+ * @swagger
+ * tags:
+ *   name: Admin
+ *   description: API endpoints related to admins
+ */
+
+/**
+ * @swagger
+ * /admin:
+ *   get:
+ *     summary: Get admin token
+ *     description: Returns a token for an admin
+ *     tags: [Admin]
+ *     responses:
+ *       200:
+ *         description: Token
+ *         content:
+ *             schema:
+ *               type: string
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     description: The user ID
+ *                   name:
+ *                     type: string
+ *                     description: The user name
+ */
 router.post("/user", authController.getToken);
 router.post("/admin", authController.getAdminToken);
 
