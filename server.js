c8f2db5571afb99af0d457b6b33ccb144058de8a
Adrian Rodriguez <rgadrian.93@gmail.com>
2024-07-28 17:32:41 -0300

    fix error

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const path = require("path");
 const app = express();
 
 const swaggerDocument = YAML.load(
-  path.join(__dirname, "docs", "documentation.yaml")
+  path.join(__dirname, "documentation.yaml")
 );
 
 const allowedOrigins = ["https://frontend-test-delta-sooty.vercel.app"];
